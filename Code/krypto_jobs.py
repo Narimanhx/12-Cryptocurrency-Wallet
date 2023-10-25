1ac0821113167bc3f8737ab265f513a5f41a28aa
nariman <nariman@narimans-mbp.home>
2023-10-24 21:19:46 -0400

    ASSIGNMENT13--09-28 _ MODULE 19 - Cryptocurrency Wallet

--- Code/krypto_jobs.py
+++ Code/krypto_jobs.py
@@ -25,6 +25,8 @@
 
 ################################################################################
 # Imports
+import sys
+import os
 import streamlit as st
 from dataclasses import dataclass
 from typing import Any, List
@@ -80,7 +82,7 @@ w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
-# YOUR CODE HERE
+from crypto_wallet import generate_account, get_balance, send_transaction
 
 ################################################################################
 # KryptoJobs2Go Candidate Information
@@ -90,28 +92,29 @@ w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 candidate_database = {
     "Lane": [
         "Lane",
-        "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0",
+        "0x2DC2594761Ad976523757Fca73B8D95064da68Cc",
         "4.3",
         0.20,
         "Images/lane.jpeg",
+        
     ],
     "Ash": [
         "Ash",
-        "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396",
+        "0x7B4a2f3eB1B46a2D59496F70416085e574358f7b",
         "5.0",
         0.33,
         "Images/ash.jpeg",
     ],
     "Jo": [
         "Jo",
-        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "0xccB051573b34cea044dCCC8830178CdE98054d59",
         "4.7",
         0.19,
         "Images/jo.jpeg",
     ],
     "Kendall": [
         "Kendall",
-        "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45",
+        "0xcAAb203E606B19615D259765D2A4A7F18fB5630F",
         "4.1",
         0.16,
         "Images/kendall.jpeg",
@@ -146,6 +149,7 @@ st.text(" \n")
 ################################################################################
 # Streamlit Sidebar Code - Start
 
+
 st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 ##########################################
@@ -156,7 +160,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
@@ -172,8 +176,9 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+ether = get_balance(w3, account.address)
 
+st.sidebar.write(ether)
 ##########################################
 
 # Create a select box to chose a FinTech Hire candidate
@@ -263,11 +268,11 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
+wage = candidate_database[person][3] * hours 
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage, " ETH")
 
 ##########################################
 # Step 2 - Part 2:
@@ -294,7 +299,7 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    transaction_hash = send_transaction(w3, account, candidate_address, wage)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
